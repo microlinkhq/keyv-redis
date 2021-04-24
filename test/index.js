43f47bd57a1a87d7a1129c20266ab48181ad4bfd
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-04-24 18:05:28 +0200

    test: remove unnecessary deps

--- test/index.js
+++ test/index.js
@@ -1,10 +1,11 @@
 'use strict'
 
 const { default: keyvTestSuite } = require('@keyv/test-suite')
-const KeyvRedis = require('this')
 const Redis = require('ioredis')
 const Keyv = require('keyv')
 const test = require('ava')
+
+const KeyvRedis = require('..')
 
 const { REDIS_HOST = 'localhost' } = process.env
 const redisURI = `redis://${REDIS_HOST}`
@@ -15,11 +16,13 @@ keyvTestSuite(test, Keyv, store)
 test('reuse a redis instance', async t => {
   const redis = new Redis(redisURI)
   redis.foo = 'bar'
+
   const keyv = new KeyvRedis(redis)
   t.is(keyv.redis.foo, 'bar')
 
   await keyv.set('foo', 'bar')
   const value = await redis.get('foo')
   t.true(value === 'bar')
+
   t.true((await keyv.get('foo')) === value)
 })
